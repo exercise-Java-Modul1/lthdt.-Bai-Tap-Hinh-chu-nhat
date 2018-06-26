031d123fadc505246e7ff78073dcaed8d5ab3b85
1234567890 <phantiensy195@gmail.com>
2018-06-26 15:57:23 +0700

    MainRectangle.lthdt. Voi Hai Truong Hop Co Dinh

--- src/MainRectangle.java
+++ src/MainRectangle.java
@@ -1,16 +1,16 @@
-import java.util.Scanner;
-
 public class MainRectangle {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Input the width: ");
-        double width = sc.nextDouble();
-        System.out.print("Input the height: ");
-        double height = sc.nextDouble();
 
-        Rectangle rec = new Rectangle(width, height);
+        double width1 = 4;
+        double height1 = 40;
+        Rectangle rec1 = new Rectangle(width1, height1);
+        System.out.println("Diện tích hình chữ nhật với width = " + width1 + " và height = " + height1 + " là: " + rec1.getArea());
+        System.out.println("Chu vi hình chữ nhật với width = " + width1 + " và height = " + height1 + " là: " + rec1.getPerimeter());
 
-        System.out.println("Diện tích hình chữ nhật với width = " + width + " và height = " + height + " là: " + rec.getArea());
-        System.out.println("Chu vi hình chữ nhật với width = " + width + " và height = " + height + " là: " + rec.getPerimeter());
+        double width2 = 3.5;
+        double height2 = 35.9;
+        Rectangle rec2 = new Rectangle(width2, height2);
+        System.out.println("Diện tích hình chữ nhật với width = " + width2 + " và height = " + height2 + " là: " + rec2.getArea());
+        System.out.println("Chu vi hình chữ nhật với width = " + width2 + " và height = " + height2 + " là: " + rec2.getPerimeter());
     }
 }
